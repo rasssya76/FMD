7eddccf291127ee60d7517f0c6f75c713a3d3bd6
Adebowale Ayodeji <theadebai@gmail.com>
2022-08-26 08:41:50 +0100

    Update _afk.js

--- plugins/_afk.js
+++ plugins/_afk.js
@@ -4,7 +4,7 @@ handler.before = function (m) {
 	let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
         this.sendButton(m.chat, `
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
+You quit AFK${user.afkReason ? ' after ' + user.afkReason : ''}
 Selama ${this.clockString(new Date - user.afk)}
 `.trim(), wm, `Menu`, `.menu`, m)
         user.afk = -1
@@ -18,9 +18,9 @@ Selama ${this.clockString(new Date - user.afk)}
         if (!afkTime || afkTime < 0) continue
         let reason = user.afkReason || ''
         this.sendButton(m.chat, `
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${this.clockString(new Date - afkTime)}
+Don't tag him!
+He was AFK ${reason ? 'on the grounds that ' + reason : 'for no reason'}
+for ${this.clockString(new Date - afkTime)}
 `.trim(), wm, 'Menu', '.menu', m)
     }
     return true
