a8ed71bdbf091546bbf6ba045187b2aa409d105f
Adebowale Ayodeji <theadebai@gmail.com>
2022-08-26 09:20:08 +0100

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -6,10 +6,10 @@ handler.before = async (m) => {
     if (chat.simi && !chat.isBanned && !m.fromMe) {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
-        let res = await fetch(global.API('https://api-sv2.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "id", cf: false }, ''))
-        if (!res.ok) throw 'maaf simi lagi sibuk'
+        let res = await fetch(global.API('https://api-sv2.simsimi.net', '/v2/', { text: encodeURIComponent(m.text), lc: "en", cf: false }, ''))
+        if (!res.ok) throw 'Sorry, I\'m currently busy.'
         let json = await res.json()
-        if (json.success == 'lu ngomong apa sih') await m.reply("gapaham cok :'v")
+        if (json.success == 'What the heck are you talking about?') await m.reply("What the heck are you talking about?")
         await m.reply(`${json.success}`)
    // m.reply(`${json.success}`)
         return !0
