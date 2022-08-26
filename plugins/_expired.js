4070b80770e709fc9e5e8aec2af86cbfa1ddd4a9
Adebowale Ayodeji <theadebai@gmail.com>
2022-08-26 09:04:18 +0100

    Update _expired.js

--- plugins/_expired.js
+++ plugins/_expired.js
@@ -1,5 +1,5 @@
 let handler = m => m
-handler.before = async function (m) {
+handler.before = async function (m) { return;
     const fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./src/logo.jpg'), thumbnail: require('fs').readFileSync('./src/logo.jpg'),sendEphemeral: true}}}
 
     if (m.isGroup && global.db.data.chats[m.chat].expired != 0) {
